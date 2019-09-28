0eb2c8bc5b95956c9ae6055b9cef95f5376ca861
jabruhns <bruhnsjames@gmail.com>
2019-09-27 20:45:35 -0700

    Commit 2

--- **PyBank**/main.py
+++ **PyBank**/main.py
@@ -1,7 +1,7 @@
 import os
 import csv
 
-bank_csv_path = os.path.join("budget_data.csv")
+bank_csv_path = os.path.join("..", "Resources", "budget_data.csv")
 with open(bank_csv_path, newline="") as csvfile:
     read_bank = csv.reader(csvfile, delimiter = ",")
     print(read_bank)
--- **PyPoll**/main.py
+++ **PyPoll**/main.py
@@ -1,3 +1,7 @@
 import os
 import csv
 
+poll_csv_path = os.path.join("..", "Resources", "election_data.csv")
+with open(poll_csv_path, newline="") as csvfile:
+        read_poll = csv.reader(csvfile, delimiter = ",")
+        print (read_poll)
