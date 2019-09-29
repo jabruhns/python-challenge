f8fb0e0c793913b6c08e52c6c2c2701c43dd172f
jabruhns <bruhnsjames@gmail.com>
2019-09-29 15:02:06 -0700

    Building

--- **PyBank**/main.py
+++ **PyBank**/main.py
@@ -2,6 +2,12 @@ import os
 import csv
 
 bank_csv_path = os.path.join("..", "Resources", "budget_data.csv")
+
 with open(bank_csv_path, newline="") as csvfile:
     read_bank = csv.reader(csvfile, delimiter = ",")
-    print(read_bank)
+    
+def print_values(read_bank):
+    date = str(read_bank[0])
+    prof_loss = int(read_bank[1])
+
+print(read_bank)
--- **PyPoll**/main.py
+++ **PyPoll**/main.py
@@ -2,6 +2,13 @@ import os
 import csv
 
 poll_csv_path = os.path.join("..", "Resources", "election_data.csv")
+
 with open(poll_csv_path, newline="") as csvfile:
         read_poll = csv.reader(csvfile, delimiter = ",")
-        print (read_poll)
+
+def print_values(read_poll):
+    voter_id = int(read_poll[0])
+    county = str(read_poll[1])
+    candidate = str(read_poll[2])
+
+print(read_poll)
