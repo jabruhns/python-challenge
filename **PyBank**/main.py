70d5f4ebe98ee23e1303dddbb622f7011d3fc583
jabruhns <bruhnsjames@gmail.com>
2019-09-30 15:14:38 -0700

    fixed

--- **PyBank**/main.py
+++ **PyBank**/main.py
@@ -1,13 +1,30 @@
 import os
 import csv
+import pandas as pd
+import numpy as np
 
 bank_csv_path = os.path.join("..", "Resources", "budget_data.csv")
 
-with open(bank_csv_path, newline="") as csvfile:
-    read_bank = csv.reader(csvfile, delimiter = ",")
-    
+read_bank = pd.read_csv(bank_csv_path)
+for row in read_bank:
+    all_months = read_bank.count
+
 def print_values(read_bank):
     date = str(read_bank[0])
-    prof_loss = int(read_bank[1])
+    change = int(read_bank[1])
 
-print(read_bank)
+total_months = read_bank['Date'].count()
+net_total = read_bank['Profit/Losses'].sum()
+avg_chg = read_bank['Profit/Losses'].mean()
+great_up = read_bank['Profit/Losses'].max()
+great_down = read_bank['Profit/Losses'].min()
+
+print("'''''''")
+print("Financial Analysis")
+print("-------------------------")
+print("Total months: " + str(total_months))
+print("Total: $" + str(net_total))
+print("Av. Change: $" + str(avg_chg))
+print("Greatest Increase in Profits: $" + str(great_up))
+print("Greatest Decrease in Profits: $" + str(great_down))
+print("'''''''")
--- **PyPoll**/main.py
+++ **PyPoll**/main.py
@@ -3,7 +3,6 @@ import pandas as pd
 import csv
 import numpy as np
 
-
 def print_values(read_poll):
     voter_id = int(read_poll[0])
     county = str(read_poll[1])
@@ -15,5 +14,5 @@ poll_csv_path = os.path.join("..", "Resources", "election_data.csv")
 read_poll = pd.read_csv(poll_csv_path)
 for row in read_poll:
         total_votes = read_poll.count
-        
+
 print(total_votes)
