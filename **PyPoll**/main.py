fdd9c4821c944378fce04c2923fe320eb33c26be
jabruhns <bruhnsjames@gmail.com>
2019-09-29 17:03:08 -0700

    progress

--- **PyPoll**/main.py
+++ **PyPoll**/main.py
@@ -1,14 +1,19 @@
 import os
+import pandas as pd
 import csv
+import numpy as np
 
-poll_csv_path = os.path.join("..", "Resources", "election_data.csv")
-
-with open(poll_csv_path, newline="") as csvfile:
-        read_poll = csv.reader(csvfile, delimiter = ",")
 
 def print_values(read_poll):
     voter_id = int(read_poll[0])
     county = str(read_poll[1])
     candidate = str(read_poll[2])
 
-print(read_poll)
+
+poll_csv_path = os.path.join("..", "Resources", "election_data.csv")
+
+read_poll = pd.read_csv(poll_csv_path)
+for row in read_poll:
+        total_votes = read_poll.count
+        
+print(total_votes)
